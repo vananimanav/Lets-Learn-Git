7a98bea929819cfd0daaf0f418d4108cd9394397
Manav <manav@bbd.co.za>
2023-01-12 15:58:05 +0530

    Increment Functionality

---
+++ Increment.java
@@ -0,0 +1,10 @@
+public class Increment{
+    public static void Main(String[] args)
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("*");
+            i++;
+        }
+    }
+}
