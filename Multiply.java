0d8699b625ac62258f9b6bd5c447a914b8a2277d
Manav <manav@bbd.co.za>
2023-01-12 11:49:59 +0530

    Feature working is on multiply integers

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void Main(String[] args){
+        int d = 4;
+        int e = 6;
+        int f = d * e;
+        System.out.println("The multiply of two number is:" + f );
+    }
+}
